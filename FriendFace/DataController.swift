7fdff2537fe880092f6d3c860fd0fe62df69d37b
derya <derya@boonandmoil.dev>
2022-11-17 10:56:39 +0000

    initialise container

---
+++ FriendFace/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataController.swift
+//  FriendFace
+//
+//  Created by Derya Antonelli on 17/11/2022.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "FriendFace")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error)")
+            }
+        }
+    }
+    
+}
